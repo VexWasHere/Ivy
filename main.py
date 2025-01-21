908904f8935d9c55de652ec50ffc9ad26886573f
Vex Harper <98134952+VexWasHere@users.noreply.github.com>
2025-01-20 19:01:47 -0500

    Add basic Gemini API configuration

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
-import platform
-import tkinter as tk
-from tkinter import *
-import customtkinter as ctk
-import speech_recognition as sr
-from win11toast import toast
+import google.generativeai as genai
 
-def speak():
-    r = sr.Recognizer()
-    print("Active!")
-
-    with sr.Microphone(1) as source:
-        audio_data = r.record(source, duration=10)
-        print("Processing...")
-        text = r.recognize_google(audio_data)
-        print(text)
-
-def check_windows_version():
-    os_version = platform.version()
-    major_version = platform.version().split('.')[0]
-    build_number = int(platform.version().split('.')[2])
-
-    if major_version == "10":
-        if build_number >= 22000:
-            return "Windows 11"
-        else:
-            return "Windows 10"
-    else:
-        return "Not Windows 10 or 11"
-
-def send_notif():
-    windows_version = check_windows_version()
-    if "Not Windows 10 or 11" in windows_version:
-        print("Not Windows 10 or 11")
-    elif "11" in windows_version:
-        print("Running on Windows 11")
-    elif "10" in windows_version:
-        print("Running on Windows 10")
-
-def main():
-
-if __name__ == '__main__':
-    main()
+genai.configure(api_key="AIzaSyAzUb-jta-rZH9VoiWaWwz50nBJxCdvNaI")
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Explain how AI works")
+print(response.text)
