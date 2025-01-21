a648683227930f256005f1a2607c87d2a419e930
Vex Harper <98134952+VexWasHere@users.noreply.github.com>
2025-01-20 19:58:18 -0500

    Updated Gemini API (Now named Ivy)

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import google.generativeai as genai
+import json
 
+#REMEMBER TO REMOVE API KEY / TRANSFER
+
+#Setting up AI
 genai.configure(api_key="AIzaSyAzUb-jta-rZH9VoiWaWwz50nBJxCdvNaI")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
+
+model = genai.GenerativeModel(
+    model_name = "gemini-1.5-flash",
+    system_instruction = "You are a personal assistant named Ivy. You give short, meaningful responses. Your goal is to support me in every way.")
+
+chat = model.start_chat()
+
+print("Welcome to Ivy! Type /end to quit")
+
+while True:
+    user_input = input("You: ")
+    user_input = user_input.lower
+
+    if "/end" in user_input:
+        return 0
+    else:
+        response = chat.send_message(user_input)
+        print(f"Ivy: {response.text}")
+
+
